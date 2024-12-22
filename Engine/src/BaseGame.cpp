e25940dce70eb35d6c442b96137d4daccc5a0737
Jelle Adyns <137185777+JelleAdyns@users.noreply.github.com>
2024-12-22 22:36:26 +0100

    Making sure Window is fully set before constructor of Game is called

--- Engine/src/BaseGame.cpp
+++ Engine/src/BaseGame.cpp
@@ -4,16 +4,16 @@
 namespace jela
 {
 
-    void BaseGame::Initialize()
-    {
-        ENGINE.SetTitle(_T("Game"));
-        ENGINE.SetColor(RGB(255, 255, 255));
-        ENGINE.SetWindowDimensions(1200, 720);
-        ENGINE.SetBackGroundColor(RGB(0, 0, 0));
-        ENGINE.SetFrameRate(60);
-    }
-
-    void BaseGame::Cleanup()
-    {
-    }
+    //void BaseGame::Initialize()
+    //{
+    //    ENGINE.SetTitle(_T("Game"));
+    //    ENGINE.SetColor(RGB(255, 255, 255));
+    //    ENGINE.SetWindowDimensions(1200, 720);
+    //    ENGINE.SetBackGroundColor(RGB(0, 0, 0));
+    //    ENGINE.SetFrameRate(60);
+    //}
+    //
+    //void BaseGame::Cleanup()
+    //{
+    //}
 }
